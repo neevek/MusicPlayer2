1833de85d9605bc04e8e22f2a5fbdeae763aad27
neevek <i@neevek.net>
2014-07-18 00:54:37 +0800

    step03_gridview_and_adapter

--- src/com/example/musicplayer/MainActivity.java
+++ src/com/example/musicplayer/MainActivity.java
@@ -1,52 +1,63 @@
 package com.example.musicplayer;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
 
-public class MainActivity extends Activity implements View.OnClickListener {
-    /**
-     * Called when the activity is first created.
-     */
+public class MainActivity extends Activity {
+    private GridView mGvMain;
+    private GridViewItemAdapter mAdapter;
+
+    private final static String[] MENU_ITEM_TEXT = new String[]{"全部音乐", "歌手", "专辑"};
+    private final static int[] MENU_ITEM_DATA_COUNT = new int[3];
+    private final static int[] MENU_ITEM_ICON = new int[] { R.drawable.icon_music, R.drawable.icon_artist, R.drawable.icon_album };
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        Button btnOpenDialer = (Button)findViewById(R.id.btn_open_dialer);
-        btnOpenDialer.setOnClickListener(this);
-
-        Button btnOpenBrowser = (Button)findViewById(R.id.btn_open_browser);
-        btnOpenBrowser.setOnClickListener(this);
-
-        Button btnOpenNextActivity = (Button)findViewById(R.id.btn_open_next_activity);
-        btnOpenNextActivity.setOnClickListener(this);
+        mGvMain = (GridView)findViewById(R.id.gv_main);
+        mAdapter = new GridViewItemAdapter();
+        mGvMain.setAdapter(mAdapter);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_open_dialer: {
-                Intent intent = new Intent(Intent.ACTION_DIAL);
-                intent.setData(Uri.parse("tel:02066666666"));
-                startActivity(intent);
-                break;
+    class GridViewItemAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return MENU_ITEM_TEXT.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return MENU_ITEM_TEXT[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.gv_main_item, null);
+
+                convertView.setTag(convertView);
             }
 
-            case R.id.btn_open_browser: {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("http://www.google.com"));
-                startActivity(intent);
-                break;
-            }
+            TextView tvMenuItem = (TextView)convertView;
+            BitmapDrawable bitmapDrawable = (BitmapDrawable)getResources().getDrawable(MENU_ITEM_ICON[position]);
+            bitmapDrawable.setBounds(0, 0, bitmapDrawable.getIntrinsicWidth(), bitmapDrawable.getIntrinsicHeight());
+            tvMenuItem.setCompoundDrawables(null, bitmapDrawable, null, null);
+            tvMenuItem.setText(MENU_ITEM_TEXT[position] + "(" + MENU_ITEM_DATA_COUNT[position] + ")");
 
-            case R.id.btn_open_next_activity: {
-                startActivity(new Intent(this, NextActivity.class));
-                break;
-            }
+            return convertView;
         }
     }
 }
