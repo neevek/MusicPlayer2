cc4dc9ec5f6c8542b0553a68426fb6c446914f96
neevek <i@neevek.net>
2014-07-31 23:17:02 +0800

    step11_executing_native_methods_from_webview

--- src/com/example/musicplayer/AboutActivity.java
+++ src/com/example/musicplayer/AboutActivity.java
@@ -1,10 +1,13 @@
 package com.example.musicplayer;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
+import android.webkit.*;
+import android.widget.Toast;
 
 /**
  * Created by neevek on 7/31/14.
@@ -19,6 +22,8 @@ public class AboutActivity extends Activity {
 
         initWebView();
 
+        mWebView.addJavascriptInterface(new MyJavascriptInterface(), "myjsobject");
+
         mWebView.loadUrl("file:///android_asset/hello.html");
 
 //        String html = "<html><body>A simple <b>WebView</b> demo</body></html>";
@@ -31,6 +36,7 @@ public class AboutActivity extends Activity {
         mWebView.setHorizontalScrollBarEnabled(false);
         mWebView.setScrollbarFadingEnabled(true);
         mWebView.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);
+        mWebView.setWebChromeClient(new MyWebChromeClient());
 
         WebSettings webSettings = mWebView.getSettings();
 
@@ -49,4 +55,36 @@ public class AboutActivity extends Activity {
     }
 
 
+    class MyJavascriptInterface {
+
+        // Caution: If you've set your targetSdkVersion to 17 or higher,
+        // you must add the @JavascriptInterface annotation to any method
+        // that you want available your web page code (the method must also be public).
+        // If you do not provide the annotation, then the method will not
+        // accessible by your web page when running on Android 4.2 or higher.
+        @JavascriptInterface
+        public String getAndroidVersion() {
+            return Build.VERSION.RELEASE;
+        }
+
+        @JavascriptInterface
+        public void showToast(String msg) {
+            Toast.makeText(AboutActivity.this, msg, Toast.LENGTH_SHORT).show();
+        }
+
+        @JavascriptInterface
+        public void openSettingsActivity() {
+            startActivity(new Intent(android.provider.Settings.ACTION_SETTINGS));
+        }
+    }
+
+    class MyWebChromeClient extends WebChromeClient {
+        @Override
+        public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+            new AlertDialog.Builder(view.getContext())
+                    .setMessage(message).setCancelable(true).show();
+            result.confirm();
+            return true;
+        }
+    }
 }
